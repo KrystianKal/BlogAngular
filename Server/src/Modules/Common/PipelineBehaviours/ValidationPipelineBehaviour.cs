7d578dc27ed020a575eb32b33dba28d7580151f2
Krystian <krystiankal96@gmail.com>
2025-01-12 20:30:13 +0100

    Simplify Validation Pipeline logic

--- Server/src/Modules/Common/PipelineBehaviours/ValidationPipelineBehaviour.cs
+++ Server/src/Modules/Common/PipelineBehaviours/ValidationPipelineBehaviour.cs
@@ -3,32 +3,26 @@ using MediatR;
 
 namespace BlogBackend.Modules.Common.PipelineBehaviours;
 
-public sealed class ValidationPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+public sealed class ValidationPipelineBehaviour<TRequest, TResponse>(
+    IEnumerable<IValidator<TRequest>> validators,
+    ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
     where TRequest : notnull
 {
-    private readonly IEnumerable<IValidator<TRequest>> _validators;
-    private readonly ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> _logger;
-
-    public ValidationPipelineBehaviour(IEnumerable<IValidator<TRequest>> validators, ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> logger)
-    {
-        _validators = validators;
-        _logger = logger;
-    }
+    private readonly ILogger<ValidationPipelineBehaviour<TRequest, TResponse>> _logger = logger;
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var context = new ValidationContext<TRequest>(request);
 
-        if (_validators.Any())
+        if (!validators.Any()) return await next();
+        
+        var validationResults = await Task.WhenAll(validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+
+        var invalidResults = validationResults.Where(x => !x.IsValid).SelectMany(x => x.Errors).ToList();
+        if (invalidResults.Count != 0)
         {
-            var validationResults = await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
-
-            var invalidResults = validationResults.Where(x => !x.IsValid).SelectMany(x => x.Errors).ToList();
-            if (invalidResults.Any())
-            {
-                throw new ValidationException(invalidResults);
-            }
-
+            throw new ValidationException(invalidResults);
         }
         return await next();
     }
